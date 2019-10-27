6f1cefe9219ee1fc2787d05fddf3181455697bb5
LukeBoyer <boyer.l@husky.neu.edu>
2019-10-27 11:59:13 -0400

    typo

--- api/src/main/java/com/codeforcommunity/validation/RequestValidator.java
+++ api/src/main/java/com/codeforcommunity/validation/RequestValidator.java
@@ -5,7 +5,7 @@ import io.vertx.core.http.HttpServerRequest;
 public interface RequestValidator {
 
   /**
-   * Validates a the form of an http request made to our server.
+   * Validates the form of an http request made to our server.
    * @param req request object.
    * @return true iff valid request.
    */
