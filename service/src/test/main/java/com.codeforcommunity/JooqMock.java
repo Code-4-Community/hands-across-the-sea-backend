6f10f2e2ba3a35548366ed25b61f8cb7b9da7ec9
Conner Nilsen <nilsen.c@husky.neu.edu>
2019-11-24 13:08:10 -0500

    Finish first iteration of jooq testing framework

--- service/src/test/main/java/com.codeforcommunity/JooqMock.java
+++ service/src/test/main/java/com.codeforcommunity/JooqMock.java
@@ -1,9 +1,12 @@
 package main.java.com.codeforcommunity;
 
+import static org.jooq.generated.Tables.NOTE;
+
 import java.sql.SQLException;
 import java.util.List;
 import org.jooq.DSLContext;
 import org.jooq.Record;
+import org.jooq.Record4;
 import org.jooq.Result;
 import org.jooq.SQLDialect;
 import org.jooq.impl.DSL;
@@ -11,14 +14,16 @@ import org.jooq.tools.jdbc.*;
 import java.lang.String;
 import java.util.Map;
 import java.util.HashMap;
+import java.util.function.Supplier;
+import org.jooq.generated.tables.Note;
 
 public class JooqMock implements MockDataProvider {
   // Results given by the user
-  private Map<String, Result<Record>> givenResults;
+  private Map<String, Supplier<Result<? extends Record>>> givenResultHandlers;
   // Results default to this class
-  private Map<String, Result<Record>> defaultResults;
+  private Map<String, Supplier<Result<? extends Record>>> defaultResultHandlers;
   // Default result to return if nothing is matched
-  private Result<Record> basicDefault;
+  private Supplier<Result<? extends Record>> basicDefaultHandler;
   // DSL Context to use
   private DSLContext context;
 
@@ -27,190 +32,254 @@ public class JooqMock implements MockDataProvider {
    */
   public JooqMock() {
     setup();
-    givenResults = new HashMap<String, Result<Record>>();
-    givenResults.put("DEFAULT", basicDefault);
+    givenResultHandlers = new HashMap<String, Supplier<Result<? extends Record>>>();
+    givenResultHandlers.put("DEFAULT", basicDefaultHandler);
   }
 
   /**
    * Default param constructor for JooqMock.
    *
-   * @param defaultResult The result to be returned if no pre-made results match what is requested.
+   * @param defaultResult The result handler to be called if no pre-made results match what is
+   * requested.
    */
-  public JooqMock(Result<Record> defaultResult) {
+  public JooqMock(Supplier<Result<? extends Record>> defaultResult) {
     this();
-    givenResults.replace("DEFAULT", defaultResult);
+    givenResultHandlers.replace("DEFAULT", defaultResult);
   }
 
   /**
-   * Constructor for JooqMock that sets the result for 'SELECT name ...'.
-   * If name is "DEFAULT", will be used as default result if not results match what is requested.
+   * Constructor for JooqMock that sets the result handler for 'SELECT name ...'.
+   * If name is "DEFAULT", will be used as default result handler if no results
+   *  match what is requested.
    * It is recommended to use the JooqMock(Record defaultResult) constructor in that case.
    *
    * @param name Name of table to select from in 'SELECT name'.
-   * @param namedResult Result to be returned when 'SELECT name' is called.
+   * @param namedResult Result handler to be called when 'SELECT name' is called.
    */
-  public JooqMock(String name, Result<Record> namedResult) {
+  public JooqMock(String name, Supplier<Result<? extends Record>> namedResult) {
     this();
-    if (name.equals("DEFAULT"))
-      givenResults.replace("DEFAULT", namedResult);
+    if (name.toUpperCase().equals("DEFAULT"))
+      givenResultHandlers.replace("DEFAULT", namedResult);
     else
-      givenResults.put(name, namedResult);
+      givenResultHandlers.put(name.toUpperCase(), namedResult);
   }
 
   /**
-   * Constructor for JooqMock that sets the results to return.
+   * Constructor for JooqMock that sets the result handlers to call.
    * If any results are named "DEFAULT", then that will be the default result.
    * If only one result exists in recordResults, it is recommended to instead use either the
    *  JooqMock(String name, Record namedResult) or JooqMock(Record defaultResult) constructors.
    *
-   * @param recordResults The map of String to Record, such that the desired Record will be returned
-   *  when the given String is used.
+   * @param recordResults The map of String to Record handlers, such that the desired Record
+   *  handler will be called when the given String is used.
    */
-  public JooqMock(Map<String, Result<Record>> recordResults) {
+  public JooqMock(Map<String, Supplier<Result<? extends Record>>> recordResults) {
     setup();
-    givenResults = recordResults;
+    recordResults.forEach((k, v) -> {
+      if (!k.equals(k.toUpperCase())) {
+        recordResults.remove(k);
+        recordResults.put(k.toUpperCase(), v);
+      }
+    });
+    givenResultHandlers = recordResults;
   }
 
   /**
-   * Add new named record to givenResults.
+   * Add new named record handler to givenResultHandlers.
    *
-   * @param name Name of result to add.
-   * @param record Record to be mapped to name.
+   * @param name Name of result handler to add.
+   * @param record Record handler to be mapped to name.
    * @param overwrite Whether to overwrite values if they already exist.
    * @return Whether or not this operation succeeded. 0 for failed, 1 for succeeded,
    *  2 for overwritten.
    */
-  public int addToRecordResults(String name, Result<Record> record, boolean overwrite) {
-    if (givenResults.containsKey(name) && !overwrite)
+  public int addToRecordResults(String name, Supplier<Result<? extends Record>> record,
+      boolean overwrite) {
+    if (givenResultHandlers.containsKey(name.toUpperCase()) && !overwrite)
       return 0;
-    else if (givenResults.containsKey(name)) {
-      givenResults.replace(name, record);
+    else if (givenResultHandlers.containsKey(name.toUpperCase())) {
+      givenResultHandlers.replace(name.toUpperCase(), record);
       return 2;
     }
 
-    givenResults.put(name, record);
+    givenResultHandlers.put(name.toUpperCase(), record);
     return 1;
   }
 
   /**
-   * Add new named record to givenResults. Will fail if name exists.
+   * Add new named record to givenResultHandlers. Will fail if name exists.
    *
-   * @param name Name of result to add.
-   * @param record Record to be mapped to name.
+   * @param name Name of result handler to add.
+   * @param record Record handler to be mapped to name.
    * @return Whether or not this operation succeeded. 0 for failed, 1 for succeeded.
    */
-  public int addToRecordResults(String name, Result<Record> record) {
-    return addToRecordResults(name, record, false);
+  public int addToRecordResults(String name, Supplier<Result<? extends Record>> record) {
+    return addToRecordResults(name.toUpperCase(), record, false);
   }
 
   /**
-   * Add new named records to givenResults. Will fail if name exists.
+   * Add new named record handlers to givenResultHandlers. Will fail if name exists.
    *
-   * @param newResults New results to add.
+   * @param newResults New result handlers to add.
    * @param overwrite Whether to overwrite values if they already exist.
    * @return A map of whether or not this operation succeeded. 0 for failed, 1 for succeeded,
    *  2 for overwritten.
    */
-  public Map<String, Integer> addToRecordResults(Map<String, Result<Record>> newResults,
+  public Map<String, Integer> addToRecordResults(Map<String,
+      Supplier<Result<? extends Record>>> newResults,
       boolean overwrite) {
     Map<String, Integer> result = new HashMap<>();
-    newResults.forEach((k, v) -> result.put(k, addToRecordResults(k, v, overwrite)));
+    newResults.forEach((k, v) -> result.put(k.toUpperCase(), addToRecordResults(k, v, overwrite)));
 
     return result;
   }
 
   /**
-   * Add new named records to givenResults. Will fail if name exists.
+   * Add new named record handlers to givenResultHandlers. Will fail if name exists.
    *
    * @param newResults New results to add.
    * @return A map of names to operation success. 0 for failed, 1 for succeeded.
    */
-  public Map<String, Integer> addToRecordResults(Map<String, Result<Record>> newResults) {
+  public Map<String, Integer> addToRecordResults(Map<String,
+      Supplier<Result<? extends Record>>> newResults) {
     return addToRecordResults(newResults, false);
   }
 
   /**
-   * Remove a result from givenResults. Will fail if name doesn't exist.
+   * Remove a result handler from givenResultHandlers. Will fail if name doesn't exist.
    *
-   * @param name Name of result to remove.
+   * @param name Name of result handler to remove.
    * @return 1 if successful, 0 otherwise.
    */
   public int removeFromRecordResults(String name) {
-    if (!givenResults.containsKey(name))
+    if (!givenResultHandlers.containsKey(name.toUpperCase()))
       return 0;
     else if (name.equals("DEFAULT"))
-      givenResults.replace(name, basicDefault);
+      givenResultHandlers.replace(name.toUpperCase(), basicDefaultHandler);
     else
-      givenResults.remove(name);
+      givenResultHandlers.remove(name.toUpperCase());
 
     return 1;
   }
 
   /**
-   * Remove a list of results from givenResults. Will fail if name doesn't exist.
+   * Remove a list of result handlers from givenResultHandlers. Will fail if name doesn't exist.
    *
-   * @param names A list of names to remove.
+   * @param names A list of names of handlers to remove.
    * @return a Map of Names to operation success. 0 for failed, 1 for succeeded.
    */
   public Map<String, Integer> removeFromRecordResults(List<String> names) {
     Map<String, Integer> result = new HashMap<>();
     for (String name : names) {
-      result.put(name, removeFromRecordResults(name));
+      result.put(name.toUpperCase(), removeFromRecordResults(name));
     }
 
     return result;
   }
 
   /**
-   * Sets the default results in the defaultResults map.
+   * Sets the default result handlers in the defaultResultHandlers map, creates a DSLContext, and
+   * sets the basicDefaultHandler handler.
    */
-  public void setup() {
+  private void setup() {
     context = DSL.using(SQLDialect.POSTGRES);
-    defaultResults = new HashMap<>();
-    basicDefault = context.newResult();
+    setDefaultResultHandlers();
+    basicDefaultHandler = () -> context.newResult();
   }
 
+  private void setDefaultResultHandlers() {
+    this.defaultResultHandlers = new HashMap<>();
+
+    defaultResultHandlers.put("NOTE", () -> {
+      Result<Record4<Integer, Integer, String, String>> result = context.newResult(NOTE.ID,
+          NOTE.USER_ID, NOTE.TITLE, NOTE.BODY);
+      result.add(context.newRecord(NOTE.ID, NOTE.USER_ID, NOTE.TITLE, NOTE.BODY).values(0, 0,
+          "NOTE 1", "Hello World!"));
+      return result;
+    });
+  }
+
+  /**
+   * Returns the context this class uses so that custom result handlers can be created.
+   *
+   * @return A mock DSLContext.
+   */
   public DSLContext getContext() {
     return this.context;
   }
 
-  @Override
-  public MockResult[] execute(MockExecuteContext ctx) throws SQLException {
-    MockResult[] mock = new MockResult[1];
-
-    String sql = ctx.sql();
+  /**
+   * Execute a single sql statement and return result.
+   *
+   * @param sql Statement to execute.
+   * @return MockResult requested.
+   */
+  private MockResult getResult(String sql) throws SQLException {
+    final MockResult[] mock = new MockResult[1];
 
     if (sql.toUpperCase().startsWith("DROP"))
       throw new SQLException("Statement not supported: " + sql);
 
-    givenResults.forEach((k, v) -> {
+    givenResultHandlers.forEach((k, v) -> {
       if (k.length() >= sql.length() && k.substring(0, sql.length()).equals(sql)) {
-        Result<Record> result = givenResults.get(k);
+        Result<? extends Record> result = givenResultHandlers.get(k).get();
         mock[0] = new MockResult(result.size(), result);
       }
     });
 
     if (mock[0] != null) {
-      return mock;
+      return mock[0];
     }
     else if (sql.toUpperCase().startsWith("SELECT")) {
       String[] stmt = sql.toUpperCase().split(" ");
+      String table = "";
+      boolean found = false;
 
-      Result<Record> result;
-      if (givenResults.containsKey(stmt[1]))
-        result = givenResults.get(stmt[1]);
+      for (String s : stmt) {
+        if (found) {
+          table = s.toUpperCase();
+          break;
+        }
+        if (s.toUpperCase().equals("FROM")) {
+          found = true;
+        }
+      }
 
-      else if (defaultResults.containsKey(stmt[1]))
-        result = defaultResults.get(stmt[1]);
+      Result<? extends Record> result;
+      if (givenResultHandlers.containsKey(table))
+        result = givenResultHandlers.get(table).get();
+
+      else if (defaultResultHandlers.containsKey(table))
+        result = defaultResultHandlers.get(table).get();
 
       else
-        result = givenResults.get("DEFAULT");
+        result = givenResultHandlers.get("DEFAULT").get();
 
       mock[0] = new MockResult(result.size(), result);
     }
 
-    else if (ctx.batch())
-      throw new SQLException("Statement not yet handled: " + sql);
+    return mock[0];
+  }
+
+  @Override
+  public MockResult[] execute(MockExecuteContext ctx) throws SQLException {
+    MockResult[] mock;
+
+    String sql = ctx.sql();
+
+    // Will add batch capabilities eventually
+//    if (ctx.batch()) {
+//      String[] stmts = ctx.batchSQL();
+//      mock = new MockResult[stmts.length];
+//      for (int i = 0; i < stmts.length; i++) {
+//        sql = stmts[i];
+//        mock[i] = getResult(sql);
+//      }
+//    }
+
+//    else {
+      mock = new MockResult[]{ getResult(sql) };
+//    }
 
     return mock;
   }
--- service/src/test/main/java/com.codeforcommunity/JooqTest.java
+++ service/src/test/main/java/com.codeforcommunity/JooqTest.java
@@ -1,5 +1,30 @@
 package main.java.com.codeforcommunity;
 
+import java.sql.SQLException;
+import main.java.com.codeforcommunity.JooqMock;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.jooq.tools.jdbc.MockExecuteContext;
+import org.jooq.tools.jdbc.MockResult;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class JooqTest {
+  JooqMock mock = new JooqMock();
+
+  @Test
+  public void test() {
+    MockResult result;
+    try {
+      MockExecuteContext ctx = new MockExecuteContext(new String[]{"SELECT * FROM NOTE"},
+          null);
+      result = mock.execute(ctx)[0];
+      System.out.println(result.data.format());
+    }
+    catch (SQLException e) {}
+
+
+    assertEquals(1, 1);
+  }
 
 }
