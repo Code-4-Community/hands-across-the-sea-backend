dfc5bbd19d4dd087b95ac7d0bf850c76c50422a6
Conner Nilsen <nilsen.c@husky.neu.edu>
2019-12-14 18:48:04 -0800

    Add actual tests

--- service/src/test/java/com.codeforcommunity/processor/ProcessorImplTest.java
+++ service/src/test/java/com.codeforcommunity/processor/ProcessorImplTest.java
@@ -3,10 +3,70 @@ package com.codeforcommunity.processor;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import com.codeforcommunity.JooqMock;
+import com.codeforcommunity.dto.FullNote;
+import java.util.ArrayList;
+import java.util.List;
+import org.jooq.UpdatableRecord;
+import org.jooq.generated.tables.Note;
 import org.jooq.generated.tables.records.NoteRecord;
+import org.jooq.impl.UpdatableRecordImpl;
 import org.junit.jupiter.api.Test;
 
 public class ProcessorImplTest {
+  JooqMock mockDb;
+  ProcessorImpl processor;
+
+  void setup() {
+    mockDb = new JooqMock();
+    processor = new ProcessorImpl(mockDb.getContext());
+  }
+
+  @Test
+  public void testGetAllMembers() {
+    setup();
+
+    assertEquals(0, processor.getAllMembers().size());
+  }
+
+  @Test
+  public void testGetAllNotes() {
+    setup();
+
+    List<UpdatableRecordImpl> list = new ArrayList<>();
+    for (int i = 0; i < 5; i++) {
+      NoteRecord n = new NoteRecord();
+      n.setId(i);
+      n.setUserId(0);
+      n.setTitle("Note" + i);
+      n.setBody("THIS IS A NOTE");
+
+      list.add(n);
+    }
+    mockDb.addReturn("SELECT", list);
+
+    NoteRecord n = new NoteRecord();
+    n.setId(11);
+    n.setUserId(11);
+    n.setTitle("NoTe11");
+    n.setBody("nOtE11");
+
+    mockDb.addReturn("SELECT", n);
+
+    List<FullNote> notes = processor.getAllNotes();
+
+    for (int i = 0; i < 5; i++) {
+      assertEquals(i, notes.get(i).getId());
+      assertEquals("Note" + i, notes.get(i).getTitle());
+      assertEquals("THIS IS A NOTE", notes.get(i).getContent());
+      assertEquals("10/20/2019", notes.get(i).getDate());
+    }
+
+    notes = processor.getAllNotes();
+    assertEquals(11, notes.get(0).getId());
+    assertEquals("NoTe11", notes.get(0).getTitle());
+    assertEquals("nOtE11", notes.get(0).getContent());
+    assertEquals("10/20/2019", notes.get(0).getDate());
+  }
 
 //  an example test using the JooqMock mock db
 //  @Test
