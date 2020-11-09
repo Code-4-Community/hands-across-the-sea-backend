2cab182462d642ece777c12e8ea21516a7a7b21a
Justin Konecny <konecnyjustin@gmail.com>
2020-11-09 08:36:32 -0500

    Fix sql

--- persist/src/main/resources/db/migration/V1__Initial_Import.sql
+++ persist/src/main/resources/db/migration/V1__Initial_Import.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS users
     first_name      VARCHAR(36) NOT NULL,
     last_name       VARCHAR(36) NOT NULL,
     privilege_level VARCHAR(16) NOT NULL,
-    password_hash   BYTEA       NOT NULL,
+    password_hash   BYTEA       NOT NULL
 );
 
 
