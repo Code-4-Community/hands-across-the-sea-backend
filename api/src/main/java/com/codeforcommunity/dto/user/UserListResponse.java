b1d0458286f7f6c6742a5c7734b7c19fcda9ab3f
Juan Tavera <jtavera235@gmail.com>
2021-04-07 16:32:17 -0400

    Removed count variable

--- api/src/main/java/com/codeforcommunity/dto/user/UserListResponse.java
+++ api/src/main/java/com/codeforcommunity/dto/user/UserListResponse.java
@@ -5,24 +5,13 @@ import java.util.List;
 
 public class UserListResponse {
 
-  private int count;
   private List<UserDataResponse> users;
 
   public UserListResponse(List<UserDataResponse> users) {
     if (users == null) {
       throw new IllegalArgumentException("Given `null` list of users");
     }
-
     this.users = users;
-    this.count = users.size();
-  }
-
-  public int getCount() {
-    return count;
-  }
-
-  public void setCount(int count) {
-    this.count = count;
   }
 
   public List<UserDataResponse> getUsers() {
